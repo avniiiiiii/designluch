0bead65aeee2072dc95cb075c0499134237fd70e
avni <avniiiiiii07@gmail.com>
2024-03-25 14:56:16 +0530

    yes

---
+++ webassignment/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar-section">
+        <div className="logo-name">Designluch</div>
+        <ul>
+          <li>About</li>
+          <li>Features</li>
+          <li>Blog</li>
+          <li>Pricing</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
