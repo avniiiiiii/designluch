b3de0110ff6ddecb2b4787a881d1d13a081dc4aa
avni <avniiiiiii07@gmail.com>
2024-03-25 16:14:00 +0530

    yes

--- webassignment/src/components/Herosection.jsx
+++ webassignment/src/components/Herosection.jsx
@@ -1,4 +1,13 @@
 function Herosection() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <div>
+          Coding fascinates us. However, imagine there also being an AI to
+          assist with your coding?
+        </div>
+      </div>
+    </>
+  );
 }
 export default Herosection;
